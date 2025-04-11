89e01a3cc6b30a8e68656d5a7b6acc854c684b70
KavinduLakshanFernando <kavindu2003fernando@gmail.com>
2025-04-11 22:07:50 +0530

    50% done

---
+++ src/main/java/org/example/delivermanagementsystem/entity/Customer.java
@@ -0,0 +1,48 @@
+package org.example.delivermanagementsystem.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "cid", columnDefinition = "VARCHAR(36)", unique = true, nullable = false)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private UUID cid;
+
+    private String fname;
+
+
+    private String lname;
+
+    private String dateOfBirth;
+
+    private String gender;
+
+    private String streetAddress;
+
+    private String city;
+
+    @OneToOne
+    @JoinColumn(name = "user_id",referencedColumnName = "uid")
+    private User user;
+
+
+    @OneToMany(mappedBy = "customer" ,cascade = CascadeType.ALL)
+    private List<PlaceOrder> orders;
+
+
+}
